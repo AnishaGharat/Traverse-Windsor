56635de3511fc6403683a27b0361ab720e48c918
adhiraj96 <adhiraj1996@gmail.com>
2021-06-14 19:26:51 +0530

    Initial UI of Trending Places Page

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Trending_Places from './app/screens/Trending_Places';
+
+
+export default function App() {
+  return (
+    <Trending_Places />
+  );
+}
---
+++ app/screens/Trending_Places.js
@@ -0,0 +1,169 @@
+
+import React from 'react';
+import { Card, Appbar, Title, Paragraph, Button, Avatar, Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import { StyleSheet, Text, TouchableHighlight, View,SafeAreaView, Alert, Image, padding, Platform, FlatList, ScrollView} from 'react-native';
+
+
+const Trending_Places = () => {
+        const _goBack = () => console.log('Went back');
+      
+        const _handleSearch = () => console.log('Searching');
+      
+        const _handleMore = () => console.log('Shown more');
+  return (
+    <View style={styles.page}>
+    <Appbar.Header style={styles.topNavigation}>
+      <Appbar.BackAction onPress={_goBack} />
+      <Appbar.Content title="Trending Places" style={styles.written} />
+      {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
+      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+    </Appbar.Header>
+  <ScrollView>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>University Of Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/universitywindsor.jpeg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>56 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Windsor Riverfront</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/riverfront.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>52 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Caesars Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/caesors.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>46 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Old Walkerville Neighborhood</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/walkerville.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>40 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Devonshire Mall</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/devonshire.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>36 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Walkerville Brewery</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/brewery.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>34 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Jackson Park</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/jackson.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>32 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Art Gallery Of Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/artgallery.jpg')} />
+    <Card.Actions>
+    <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> 
+    <Paragraph style={styles.para}>29 check-ins</Paragraph>
+    </Card.Actions>
+  </Card>
+  </ScrollView>
+  </View>
+    
+    //     <View style={styles.topNavigation}>
+    //   <Text style={styles.written}>Trending Places</Text>
+    //   </View>
+    //   <TouchableHighlight onPress={() => console.log("Image pressed")}>
+    //   <Image onPress={() => alert('Welcome to University of Windsor')} source={
+    //     require('../assets/universitywindsor.jpeg')}/>
+    //     </TouchableHighlight>
+    //   <Button
+    //     title="Learn More"
+    //     color="blue"
+    //     accessibilityLabel="Learn more about this purple button"
+    //   />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    backgroundColor: '#b7dfdb',
+    alignItems: 'center',
+    //justifyContent: 'center',
+    resizeMode: 'contain',
+    paddingTop: Platform.OS === "android" ? 32 : 0
+  },
+  written:
+  {
+    color: '#fff',
+    alignItems: 'center',
+  },
+
+  topNavigation:
+  {
+    backgroundColor: '#0097a7',
+  },
+  page:
+  {
+    backgroundColor: '#b7dfdb',
+    flex:1,
+  },
+  button:
+  {
+    backgroundColor: '#78a834',
+  },
+  para:
+  {
+    textAlign:'right',
+  }
+  
+});
+
+export default Trending_Places;
+
+// export default function App(){
+//   const theme = {
+//     ...DefaultTheme,
+//     colors: {
+//       ...DefaultTheme.colors,
+//       primary: '#0097a7',
+//     },
+//   };
+//   return(
+//     <PaperProvider theme={theme}>
+//       <Trending_Places/>
+//     </PaperProvider>
+//   );
