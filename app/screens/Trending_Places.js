22086f1a2a96a274d165e28b373eac1b37d3d088
adhiraj96 <adhiraj1996@gmail.com>
2021-06-15 19:54:17 +0530

    Reverting changes

--- app/screens/Trending_Places.js
+++ app/screens/Trending_Places.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Card, Appbar, Title, Paragraph, Button, IconButton, Colors} from 'react-native-paper';
 import { StyleSheet, Text, TouchableHighlight, View,SafeAreaView, Alert, Image, padding, Platform, FlatList, ScrollView} from 'react-native';
 import { color } from 'jimp';
+<<<<<<< HEAD
 import Cards from '../components/TrendingPlaces-Cards.js'
 
 const trendingPlaces=[
@@ -63,6 +64,8 @@ const trendingPlaces=[
     type:'Attractions'
   }
 ]
+=======
+>>>>>>> parent of 11a27f4 (Arranged the trending places data in arrays)
 
 const Trending_Places = () => {
         const _goBack = () => console.log('Went back');
@@ -79,12 +82,127 @@ const Trending_Places = () => {
       <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
     </Appbar.Header>
   <ScrollView>
+<<<<<<< HEAD
   {trendingPlaces.map(place =>(
     <Cards title={place.title} imgSrc={place.imgSrc} numLikes={place.numLikes} numCheckins={place.numCheckins}/>
   )) } 
+=======
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>University Of Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/universitywindsor.jpeg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>81 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>56 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Windsor Riverfront</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/riverfront.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>74 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>52 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Caesars Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/caesors.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>65 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>46 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Old Walkerville Neighborhood</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/walkerville.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>59 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>40 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Devonshire Mall</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/devonshire.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>48 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>36 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Walkerville Brewery</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/brewery.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>36 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>34 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Jackson Park</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/jackson.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>25 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>21 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+  <Card style={{margin:10}}>
+    <Card.Content>
+      <Title>Art Gallery Of Windsor</Title>
+    </Card.Content>
+    <Card.Cover source={require('../assets/artgallery.jpg')} />
+    <Card.Actions>
+    <IconButton icon="heart" color={Colors.red500} size={20} onPress={() => console.log('Pressed') } />
+    <Paragraph style={styles.para}>19 likes</Paragraph>
+    {/* <Button icon="car" mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>DIRECTIONS</Button> */}
+    <Card.Content>
+    <Paragraph>20 check-ins</Paragraph>
+    </Card.Content>
+    </Card.Actions>
+  </Card>
+>>>>>>> parent of 11a27f4 (Arranged the trending places data in arrays)
   </ScrollView>
-  </View> 
-
+  </View>
+ 
   );
 }
 
