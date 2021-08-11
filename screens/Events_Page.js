9069619152b7acb16122e36e7ef69d87de8884f9
ravleen <kaur5f@uwindsor.ca>
2021-08-11 06:29:09 +0530

    Added New Event Form

--- screens/Events_Page.js
+++ screens/Events_Page.js
@@ -1,22 +1,35 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, Appbar, Title, Paragraph, Button, IconButton, Colors} from 'react-native-paper';
-import { StyleSheet, Text, TouchableHighlight, View,SafeAreaView, Alert, Image, padding, Platform, FlatList, ScrollView} from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View,SafeAreaView, Alert, Image, padding, Platform, FlatList, ScrollView,TextInput} from 'react-native';
 import { color } from 'jimp';
+import Modal from 'react-native-modal';
+import DateField from 'react-native-datefield';
+import Toast from 'react-native-simple-toast';
+
 
 const Events_Page = () => {
         const _goBack = () => console.log('Went back');
       
         const _handleSearch = () => console.log('Searching');
       
-        const _handleMore = () => console.log('Shown more');
+        const _handleMore = () => {return(<><New_Event/></>);};
+        const [isModalVisible, setModalVisible] = useState(false);
+
+        const toggleModal = () => {
+          setModalVisible(!isModalVisible);
+        };
+        const saveEvent = () => {
+          toggleModal();
+          Toast.show('Event Saved!');
+        };
   return (
     <View style={styles.page}>
     <Appbar.Header style={styles.topNavigation}>
       <Appbar.BackAction onPress={_goBack} />
       <Appbar.Content title="Events" style={styles.written} />
       {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
-      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      <Appbar.Action icon="plus" onPress={toggleModal} />
     </Appbar.Header>
   <ScrollView>
   <Card style={{margin:10}}>
@@ -115,6 +128,43 @@ const Events_Page = () => {
     </Card.Actions>
   </Card> */}
   </ScrollView>
+  <Modal isVisible={isModalVisible} backdropColor={'white'} backdropOpacity={0} style={styles.container} 
+  >
+            <Button icon="close" onPress={toggleModal} style={ {position: 'absolute',
+                            left: 250,
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                            } }></Button>
+
+          <TextInput
+      style={styles.textInput}
+      placeholder="Event Name"
+      maxLength={20}
+    />
+    <TextInput
+      style={styles.textInput}
+      placeholder="Organiser"
+      maxLength={20}
+    />
+    <TextInput
+      style={styles.textInput}
+      placeholder="Location"
+      maxLength={20}
+    />
+    <TextInput
+      style={styles.textInput}
+      placeholder="Contact"
+      maxLength={20}
+    />
+    <TextInput
+      style={styles.textInput}
+      placeholder="Date"
+      maxLength={20}
+    />
+          <Button style={styles.saveButton} onPress={saveEvent} color='#FFFFFF' >Save</Button>
+        
+      </Modal>
   </View>
  
   );
@@ -134,7 +184,33 @@ const styles = StyleSheet.create({
     color: '#fff',
     alignItems: 'center',
   },
-
+  textInput: {
+    borderColor: '#CCCCCC',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  container: {
+    
+    marginTop: 55,
+    backgroundColor: '#F5FCFF',
+    marginBottom:100,
+    marginTop:100,
+    width:300,
+    marginLeft:30,
+    height:50,
+    borderRadius:10
+  },
+  saveButton: {
+    marginTop:30,
+    backgroundColor:'#0097a7',
+    marginLeft:50,
+    marginRight:50,
+    margin: 10
+  },
   topNavigation:
   {
     backgroundColor: '#0097a7',
